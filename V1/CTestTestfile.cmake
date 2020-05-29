cd6954bcdd1601672d55a4b6d139a87fbd225e39
sambegou <ts76diallo@gmail.com>
2020-05-30 01:21:59 +0200

    Adding the project to the github

---
+++ V1/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /home/thierno/sem4/V1
+# Build directory: /home/thierno/sem4/V1
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(test1 "./testAM")
+add_test(test2 "./testSAM")
+add_test(test3 "./testSEY")
+add_test(test4 "./test_DIAK")
